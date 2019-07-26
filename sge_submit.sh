416bffda434449d3198fc7ed7fc96695ce737032
CMA <87302380@qq.com>
2019-07-26 13:10:32 +0200

    Delete sge_submit.sh

--- sge_submit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#$ -N sge_test_job
-#$ -o /home/lchen/sge_log/sge_stdout_logs/
-#$ -e /home/lchen/sge_log/sge_stderr_logs/
-
-# enter the virtual environment
-source /home/lchen/parameters/parameter/bin/activate
-python3 /home/lchen/parameters/search_parameter.py --run_id $JOB_ID --nic_name lo 
